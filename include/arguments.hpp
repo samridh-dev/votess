0141a9f867ec0d5a3d94cd402a5d1c4e32fd9146
samridh-dev <singhsam@grinnell.edu>
2024-08-05 18:47:57 +0200

    implemented new vtargs api

--- include/arguments.hpp
+++ include/arguments.hpp
@@ -1,199 +1,155 @@
-#ifndef ARGUMENTS_HPP
-#define ARGUMENTS_HPP
+#ifndef VTARGS_HPP
+#define VTARGS_HPP
 
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <variant>
-
-#define USE_NEW_ARGS 0
-
-///////////////////////////////////////////////////////////////////////////////
-/// Compile time default parameters
-///////////////////////////////////////////////////////////////////////////////
-
-/* ------------------------------------------------------------------------- */
-
-// using macros for two reasons:
+// Using macros for two reasons:
 //  * pybind11 compatibility
 //  * ability to have mixed types
 
+#define ARGS_DEFAULT_K 80
+
+#define ARGS_DEFAULT_CPU_NTHREADS 0
+#define ARGS_DEFAULT_GPU_NDWORKSIZE 1
+
+#define ARGS_DEFAULT_CHUNKSIZE 8196
+#define ARGS_DEFAULT_USE_CHUNKING false
+
+#define ARGS_DEFAULT_USE_RECOMPUTE true
+
+#define ARGS_DEFAULT_GRID_RESOLUTION 16
 #define ARGS_DEFAULT_P_MAXSIZE 128
 #define ARGS_DEFAULT_T_MAXSIZE 128
 
-/* ------------------------------------------------------------------------- */
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <stdexcept>
 
-///////////////////////////////////////////////////////////////////////////////
-/// Function specific arguments (internal)
-///////////////////////////////////////////////////////////////////////////////
 namespace args {
-
-/* ------------------------------------------------------------------------- */
-
-struct global {
-  int k;
-  int __cpu__nthreads;
-  int __gpu__ndworksize;
-  global(const int k0) : 
-    k(k0) 
-  {}
-};
-
-/* ------------------------------------------------------------------------- */
 
 struct xyzset {
   int grid_resolution;
-  xyzset(const int gr0) : 
-    grid_resolution(gr0) 
-  {}
+  xyzset(const int gr0) : grid_resolution(gr0) {}
 };
-
-/* ------------------------------------------------------------------------- */
 
 struct knn {
   int k;
   int grid_resolution;
-  knn(const int k0, const int gr0) : 
-    k(k0),
-    grid_resolution(gr0) 
-  {}
+  knn(const int k0, const int gr0) : k(k0), grid_resolution(gr0) {}
 };
-
-/* ------------------------------------------------------------------------- */
 
 struct cc {
   int k;
   int p_maxsize;
   int t_maxsize;
-  cc(const unsigned short int k0,
-      const unsigned short int pms0, const unsigned short int tms0
-  ) : k(k0), p_maxsize(pms0), t_maxsize(tms0) {}
+  cc(const int k0, const int pms0, const int tms0) 
+    : k(k0), p_maxsize(pms0), t_maxsize(tms0) {}
 };
 
-/* ------------------------------------------------------------------------- */
 } // namespace args
 
 namespace votess {
 
-#if USE_NEW_ARGS
-class vtargs {
-
+class vtargref {
   private:
-    
-    // global
-    int k;
-    int __cpu__nthreads;
-    int __gpu__ndsize;
-    int chunksize;
-    bool use_recompute;
-    
-    // knn
-    int gr;
+    std::string str;
 
-    // dnn
-    int p_maxsize;
-    int t_maxsize;
+    template <typename T>
+    std::string to_str(const T v) const {
+      std::ostringstream oss;
+      oss << v;
+      return oss.str();
+    }
 
-    struct args::xyzset xyzset;
-    struct args::knn knn;
-    struct args::cc cc;
+    template <typename T>
+    T from_str(const std::string& s) const {
+      std::istringstream iss(s); 
+      T v;
+      iss >> v;
+      if (iss.fail()) {
+        throw std::invalid_argument("Invalid type conversion");
+      }
+      return v;
+    }
   
-    using arg_t std::variant<int, bool>
-    std::unordered_map<std::string, arg_t> map;
+  public:
+    template <typename T> 
+    vtargref& operator=(const T& other) {
+      this->str = to_str(other);
+      return *this;
+    }
 
-    void init(void);
-    void update(void);
+    template <typename T>
+    operator T() const {
+      return from_str<T>(str);
+    }
+
+    template<typename T> 
+    T get() const {
+      return from_str<T>(str);
+    }
+};
+
+class vtargs {
+  private:
+    std::unordered_map<std::string, vtargref> map;
+
+    void init(void) {
+      map["k"] = ARGS_DEFAULT_K;
+      map["cpu_nthreads"] = ARGS_DEFAULT_CPU_NTHREADS;
+      map["gpu_ndsize"] = ARGS_DEFAULT_GPU_NDWORKSIZE;
+      map["chunksize"] = ARGS_DEFAULT_CHUNKSIZE;
+      map["use_chunking"] = ARGS_DEFAULT_USE_CHUNKING;
+      map["use_recompute"] = ARGS_DEFAULT_USE_RECOMPUTE;
+      map["knn_grid_resolution"] = ARGS_DEFAULT_GRID_RESOLUTION;
+      map["cc_p_maxsize"] = ARGS_DEFAULT_P_MAXSIZE;
+      map["cc_t_maxsize"] = ARGS_DEFAULT_T_MAXSIZE;
+    }
 
   public:
 
-    vtargs(void);
-    vtargs(const int _k);
+    vtargs(void) {
+      init();
+    }
 
-    arg_t operator[](const std::string& key) const;
-    arg_t& operator[](const std::string& key);
-    void operator()(const std::string& key, const arg_t& val);
-  
-};
-} // namespace votess
-
-void votess::vtargs::init(void) {
-  map["k"] = k;
-  map["cpu_nthread"] = __cpu__nthreads;
-  map["gpu_nd_worksize"] = __gpu__ndsize;
-  map["chunk_size"] = chunksize;
-  map["allow_recompute"] = chunksize;
-  map["knn_grid_resolution"] = gr;
-  map["dnn_p_maxsize"] = p_maxsize;
-  map["dnn_t_maxsize"] = t_maxsize;
-}
-
-votess::vtargs::vtargs(void) {
-  init();
-}
-
-votess::vtargs::vtargs(const int _k) {
-  init();
-  k = _k;
-}
-
-arg_t votess::vtargs::operator[](const std::string& key) const {
-  auto it = map.find(key);
-  if (it != map.end()) {
+  vtargref& operator[](const std::string& key) {
+    auto it = map.find(key);
+    if (it == map.end()) {
+      throw std::invalid_argument("key: " + key + 
+                                  " not found in file: " + __FILE__ + 
+                                   " at line: " + std::to_string(__LINE__));
+    }
     return it->second;
-  } else {
-    throw std::invalid_argument("Key not found");
-  }
-}
-
-arg_t& votess::vtargs::operator[](const std::string& key) {
-  auto it = map.find(key);
-  if (it == map.end()) {
-    throw std::invalid_argument("Key not found");
-  }
-  return it->second;
-}
-
-void votess::vtargs::operator()(const std::string& key, const arg_t& val) {
-  auto it = map.find(key);
-  if (it != map.end()) {
-    it->second = val;
-    update();
-  } else {
-    throw std::invalid_argument("Key not found");
-  }
-}
-
-#else
-struct vtargs {
-
-  struct args::global global;
-  struct args::xyzset xyzset;
-  struct args::knn knn;
-  struct args::cc cc;
-
-  const size_t nthreads;
-
-  vtargs(
-    const int _k,
-    const int _grid_resolution = 1,
-    const int _nthreads = 1,
-    const int _p_maxsize = ARGS_DEFAULT_P_MAXSIZE,
-    const int _t_maxsize = ARGS_DEFAULT_T_MAXSIZE) 
-    : global(_k),
-      xyzset(_grid_resolution),
-      knn(_k, _grid_resolution),
-      cc(_k, _p_maxsize , _t_maxsize),
-      nthreads(_nthreads) {}
-
-  void set_k(const int k) {
-    global.k = k;
-    knn.k = k;
-    cc.k = k;
   }
 
+  const vtargref& operator[](const std::string& key) const {
+    auto it = map.find(key);
+    if (it == map.end()) {
+      throw std::invalid_argument("key: " + key + 
+                                  " not found in file: " + __FILE__ + 
+                                   " at line: " + std::to_string(__LINE__));
+    }
+    return it->second;
+  }
+
+    const struct args::xyzset get_xyzset(void) const {
+      int gr = (*this)["knn_grid_resolution"];
+      return args::xyzset(gr);
+    }
+
+    const struct args::knn get_knn(void) const {
+      int gr = (*this)["knn_grid_resolution"];
+      int k = (*this)["k"];
+      return args::knn(k, gr);
+    }
+
+    const struct args::cc get_cc(void) const {
+      int k = (*this)["k"];
+      int p_maxsize = (*this)["cc_p_maxsize"];
+      int t_maxsize = (*this)["cc_t_maxsize"];
+      return args::cc(k, p_maxsize, t_maxsize);
+    }
 };
+
 } // namespace votess
-#endif // USE_NEW_ARGS
 
-
-#endif
+#endif // VTARGS_HPP
