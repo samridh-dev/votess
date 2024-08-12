137bcb8af6ee7b79ec9b24d16e8b4ac374e881be
samridh-dev <singhsam@grinnell.edu>
2024-08-12 16:08:50 +0200

    updated notation

--- include/heap.hpp
+++ include/heap.hpp
@@ -9,26 +9,26 @@
 
 namespace heap {
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void swap(
-  std::vector<T1>& heap_id,
-  std::vector<T2>& heap_pq,
+  std::vector<Ti>& heap_id,
+  std::vector<Tf>& heap_pq,
   const size_t h0,
   const size_t a, const size_t b
 );
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void maxheapify(
-  std::vector<T1>& heap_id,
-  std::vector<T2>& heap_pq,
+  std::vector<Ti>& heap_id,
+  std::vector<Tf>& heap_pq,
   const size_t h0,
   const size_t s, size_t i
 );
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void sort(
-  std::vector<T1>& heap_id,
-  std::vector<T2>& heap_pq,
+  std::vector<Ti>& heap_id,
+  std::vector<Tf>& heap_pq,
   const size_t h0,
   const size_t k
 );
@@ -41,26 +41,26 @@ inline void sort(
 
 namespace heap {
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void swap(
-  device_accessor_readwrite_t<T1>& heap_id,
-  device_accessor_readwrite_t<T2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const size_t h0,
   const size_t a, const size_t b
 );
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void maxheapify(
-  device_accessor_readwrite_t<T1>& heap_id,
-  device_accessor_readwrite_t<T2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const size_t h0,
   const size_t s, size_t i
 );
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void sort(
-  device_accessor_readwrite_t<T1>& heap_id,
-  device_accessor_readwrite_t<T2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const size_t h0,
   const size_t k
 );
@@ -69,56 +69,26 @@ inline void sort(
 
 namespace heap {
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void swap(
-  const device_accessor_readwrite_t<T1>& heap_id,
-  const device_accessor_readwrite_t<T2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const int s, const int i,
   const int a, const int b
 );
  
-
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void maxheapify(
-  const device_accessor_readwrite_t<T1>& heap_id,
-  const device_accessor_readwrite_t<T2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const int s, const int i,
   const int k, int idx
 );
 
-template <typename T1, typename T2>
+template <typename Ti, typename Tf>
 inline void sort(
-  const device_accessor_readwrite_t<T1>& heap_id,
-  const device_accessor_readwrite_t<T2>& heap_pq,
-  const int s, const int i,
-  const int k
-);
-
-} // namespace heap
-
-namespace heap {
-
-template <typename T1, typename T2>
-inline void swap(
-  const sycl::local_accessor<T1, 2>& heap_id,
-  const sycl::local_accessor<T2, 2>& heap_pq,
-  const int s, const int i,
-  const int a, const int b
-);
- 
-
-template <typename T1, typename T2>
-inline void maxheapify(
-  const sycl::local_accessor<T1, 2>& heap_id,
-  const sycl::local_accessor<T2, 2>& heap_pq,
-  const int s, const int i,
-  const int k, int idx
-);
-
-template <typename T1, typename T2>
-inline void sort(
-  const sycl::local_accessor<T1, 2>& heap_id,
-  const sycl::local_accessor<T2, 2>& heap_pq,
+  const device_accessor_readwrite_t<Ti>& heap_id,
+  const device_accessor_readwrite_t<Tf>& heap_pq,
   const int s, const int i,
   const int k
 );
